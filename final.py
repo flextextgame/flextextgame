4061fd7ca49eaccd8f07f32b12b8a2e3e4733f4b
Steven Van <SVan6771@student.pps.net>
2018-05-17 14:41:33 -0700

    Fixed involuntary shuffling | more comments

--- final.py
+++ final.py
@@ -1,14 +1,6 @@
 import random
 # Title: Flex-Text game
 
-# welcome to flextextgame
-
-# goal may 20th.. add features
-# error handling and error types
-# rock,paper, scisors
-# card
-# dice
-
 """
 Objective: Beat the CPU (Computer) in either of the three random number
 generator game. Wins and loss for each game are accumulated.
@@ -114,7 +106,6 @@ while mode == "none":
                                 if cpu == "scissor":
                                         win = win + 1
                                         print("You Win")	
-
                         if player_choice == "paper":
                                 if cpu == "paper":
                                         print("Tie")
@@ -124,7 +115,6 @@ while mode == "none":
                                 if cpu == "rock":
                                         win = win + 1
                                         print("You Win")
-
                         if player_choice == "scissor":
                                 if cpu == "scissor":
                                         print("Tie")
@@ -242,6 +232,8 @@ while mode == "none":
                         
                         #set shuffle option to *blank*                                  
                         shuffle = ""
+                        #shuffling error state set to 0
+                        shuffle_error = 0
                         #loop shuffle when *blank*
                         while shuffle == "":
                                 #13 cards defined in deck
@@ -249,13 +241,17 @@ while mode == "none":
                                 #deck multiplied to get full deck
                                 deck = deck * 4
                                 #shuffled deck set blank
-                                shuffled_deck = []
-                                #begin shuffling for 52 times
-                                for i in range(52):
-                                        shuffled_deck.append(random.choice(deck))
-                                        deck.remove(shuffled_deck[i])
-                                print(shuffled_deck)
                                 
+                                #keeps previous shuffled_deck if error in shuffle permission occurs
+                                if shuffle_error == 0:
+                                        shuffled_deck = []
+                                        #begin shuffling for 52 times
+                                        for i in range(52):
+                                                shuffled_deck.append(random.choice(deck))
+                                                deck.remove(shuffled_deck[i])                                        
+                                if shuffle_error == 1:
+                                        shuffled_deck == shuffled_deck
+                                                                      
                                 print('\n')
 
                                 #ask Player if they want to shuffle deck again
@@ -266,9 +262,11 @@ while mode == "none":
                                 if shuffle == "yes":
                                         print('\n')
                                         shuffle = ""
+                                        shuffle_error = 0
                                         continue
                                 #loops to asking to shuffle again if answer is neither "yes" or "no"
                                 if shuffle != "no":
+                                        shuffle_error = 1
                                         shuffle = ""
                                         print("Must be either of the two options in square brackets.")
                                         print('\n')
@@ -283,8 +281,6 @@ while mode == "none":
                         pick_side = ""
                         #ask Player to pick a side from shuffled_deck when spread out in imaginary hands
                         while pick_side == "":
-                                #delete
-                                print(shuffled_deck)
                                 print('\n')
                                 pick_side = input("Pick a side to pick from [left, middle, right]: ")
                                 #makes sure text-based input is applicable for string comparison
@@ -299,50 +295,50 @@ while mode == "none":
                                 if pick_side == "left":
                                         #cards from the furthest left to the right until the 18th card
                                         select_deck = shuffled_deck[:18]
-                                        #delete
-                                        print(select_deck)
                                         #asks Player to select a card from the given range inputed
                                         card_select = input("There are 18 cards. Pick one [1 to 18]: ")
+                                        #makes sure input is the correct type
                                         try:
                                                 card_select = int(card_select)
+                                        #loop to side picking if input's type is incorrect
                                         except ValueError:
                                                 pick_side = ""
                                                 continue
                                         card_select = card_select - 1
                                         
-                                #if Player picked the left side
+                                #if Player picked the middle side
                                 if pick_side == "middle":
                                         #asks Player to select a card from the 18 cards in the middle section (9 from the left to 9 from the right)
-                                        select_deck = shuffled_deck[16:34]
-                                        #delete
-                                        print(select_deck)
+                                        select_deck = shuffled_deck[16:34]                                    
                                         #asks Player to select a card from the given range inputed
                                         card_select = input("There are 18 cards. Pick one [1 to 18]: ")
+                                        #makes sure input is the correct type
                                         try:
                                                 card_select = int(card_select)
+                                        #loop to side picking if input's type is incorrect
                                         except ValueError:
                                                 pick_side = ""
                                                 continue
                                         card_select = card_select - 1
-                                        
+                                #if Player picked the right side        
                                 if pick_side == "right":
                                         #asks Player to select a card from the furthest right to the left until the 18th card
-                                        select_deck = shuffled_deck[34:]
-                                        #delete
-                                        print(select_deck)
+                                        select_deck = shuffled_deck[34:]                                        
                                         #asks Player to select a card from the given range inputed
                                         card_select = input("There are 18 cards. Pick one [1 to 18]: ")
+                                        #makes sure input is the correct type
                                         try:
                                                 card_select = int(card_select)
+                                        #loop to side picking if input's type is incorrect
                                         except ValueError:
                                                 pick_side = ""
                                                 continue
+                                        #accomodates for index as the 1st card is at position 0
                                         card_select = card_select - 1
+                                #select card according to Player's input
+                                player_select = select_deck[card_select]=
 
-                                player_select = select_deck[card_select]
-                                print(player_select)
-
-                        #Print results
+                        #Print cards selected by both Player and CPU
                         print("Player: " + str(player_select))
                         print("CPU: " + str(cpu_select))
 
@@ -365,7 +361,8 @@ while mode == "none":
                                 cpu_select = 12
                         if cpu_select == "King":
                                 cpu_select = 13
-                        
+
+                        #Player win/loss/tie print conditions
                         if player_select > cpu_select:
                                 print("You Win")
                                 win = win + 1
@@ -375,20 +372,28 @@ while mode == "none":
                         else:
                                 print("Tie")
 
+                        #prints win/loss statistics of the round
                         print("Wins: " + str(win))
                         print("Loss: " + str(loss))
 
+                        #count rounds to limit loop of gamemode
                         rounds = rounds + 1
 
                         print('\n')
 
-                #continue or change mode
+                #asks Player to continue or change gamemode
                 continue_mode = input("Continue [1] or Change Mode [2]: ")
+                #option to continue gamemode
                 if continue_mode == "1":
+                        #makes sure shuffled_deck is shuffled for new rounds of gamemode
+                    shuffle_error = 0
                     print('\n')
                     continue
+                #option to switch gamemode
                 else:
+                    #tally up wins and loss of the rounds and add to Total win/l
                     win_total = win_total + win
                     loss_total = loss_total + loss
                     print('\n')
+                    #switch text to Menu
                     mode = "none"        
